b47f0f1ad92bfd2ab0ff24a031ce932ac69714c8
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 01:53:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(argument)
+  if argument / 3 == TRUE
+    return "Fizz"
+    
+  else if argument / 5 == TRUE
+    return "Buzz"
+    
+  if argument / 3 == TRUE && argument / 5 == TRUE
+    return "FizzBuzz"
+
+  else
+  return nil
+  
+end
